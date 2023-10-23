b7fc1b60b9a6bb469308a3abbde98fa3700a3afe
Lasse Hastrup <lasse.hastrup@fellowmind.dk>
2023-10-23 16:23:32 +0200

    initial commit

---
+++ src/content_option.js
@@ -0,0 +1,102 @@
+const logotext = "Lasse";
+const meta = {
+    title: "Lasse Hastrup",
+    description: "I’m Lasse Hastrup, Platform Engineer _ Azure Cloud Consultant, currently working in Aarhus",
+};
+
+const introdata = {
+    title: "I’m Lasse Hastrup",
+    animated: {
+        first: "Technology enthusiast",
+        third: "Cloud first approach",
+        second: "Infrastructure As Code approach",
+    },
+    description: "",
+    your_img_url: "",
+};
+
+const dataabout = {
+    title: "Who am I?",
+    aboutme: "I’m Lasse Hastrup, Platform Engineer & Azure Cloud Consultant, currently working in Aarhus, Denmark. I have a passion for technology and love to learn new things. "
+};
+const worktimeline = [{
+    jobtitle: "Designer of week",
+    where: "YAdfi",
+    date: "2020",
+},
+{
+    jobtitle: "Designer of week",
+    where: "Jamalya",
+    date: "2019",
+},
+{
+    jobtitle: "Designer of week",
+    where: "ALquds",
+    date: "2019",
+},
+];
+
+const skills = [{
+    name: "Azure Infrastructure",
+    value: 90,
+},
+{
+    name: "PowerShell",
+    value: 95,
+},
+{
+    name: "Azure DevOps",
+    value: 85,
+},
+{
+    name: "Microsoft Entra",
+    value: 80,
+},
+{
+    name: "Python",
+    value: 50,
+},
+];
+
+const services = [{
+    title: "Infrastructure as Code",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+},
+{
+    title: "Mobile Apps",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+},
+{
+    title: "Wordpress Design",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+},
+];
+
+const dataportfolio = [{
+    img: "https://techcommunity.microsoft.com/t5/image/serverpage/image-id/212187i9A179776A9938109/image-size/large?v=v2&px=999",
+    description: "Azure Landing Zones",
+    link: "/portfolio/azure-landing-zones/index.html",
+}
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "l@hastrup.m,",
+    YOUR_FONE: "+45 5372 9852",
+};
+
+const socialprofils = {
+    github: "https://github.com/lassehastrup",
+    linkedin: "https://www.linkedin.com/in/lassehastrup/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
